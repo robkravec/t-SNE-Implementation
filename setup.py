f7d9ad8a3cddb0b58724ef8710ca7977c664f1e7
Rob Kravec <rkravec17@gmail.com>
2021-04-25 00:31:05 +0000

    Tidying up tests

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
 setup(name='tsne663',
       version='0.3',
       description='Implentation of the t-SNE algorithm for dimension reduction',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/robkravec/tsne663',
+      long_description = 'Please see https://github.com/robkravec/STA663_Project for more information on this package',
+      url='https://github.com/robkravec/STA663_Project',
       author='Marc Brooks, Rob Kravec, Steven Winter',
       author_email='marc.brooks@duke.edu, robert.kravec@duke.edu, steven.winter@duke.edu',
       license='MIT',
       packages=find_packages(), 
-      install_requires = ['matplotlib', 'numpy', 'numba', 'tqdm', 'scikit-learn', 'markdown', 'plotly', 'mpl_toolkits', 'textwrap'])
+      install_requires = ['matplotlib>=3.1.1', 'numpy>=1.18.1', 'numba>=0.48.0', 'tqdm>=4.42.0', 'scikit-learn>=0.22.1'])
--- tests/test.py
+++ tests/test.py
@@ -1,53 +1,55 @@
 import numpy as np
-from tsne663 import *
 np.random.seed(663)
+import sys
+sys.path.append("../tsne663")
+from .tsne import *
 
-###################################################################################
+###########################################################################################################################
 # Get random data to test
-###################################################################################
+###########################################################################################################################
 n, p, d = 10, 10, 2
 X = np.random.normal(0, 1, [n, p])
 R = np.random.gamma(1, 1, [n, n])
 Y = np.random.normal(0, 1, [n, d])
 
-###################################################################################
+###########################################################################################################################
 # Test equivalence of results for different levels of optimization
-###################################################################################
+###########################################################################################################################
 Y_dists = get_Y_dists(Y)
 
 D_none = get_dists(X, "none")
 D_fastest = get_dists(X, "fastest")
-assert np.allclose(D_none, D_fastest)
+assert np.allclose(D_none, D_fastest), "Distance functions return different values"
 
 P_none = get_P(D_fastest, optim = "none")
 P_fastest = get_P(D_fastest, optim = "fastest")
-assert np.allclose(P_none, P_fastest)
+assert np.allclose(P_none, P_fastest), "Functions to create p_ij matrices return different values"
 
 grad_none = get_grad(R, Y_dists, Y, "none")
 grad_fast = get_grad(R, Y_dists, Y, "fast")
-grad_fastest = get_grad(R, Y_dists, Y, "fastest")
-
-assert np.allclose(grad_none, grad_fast)
-
-assert np.allclose(grad_none, grad_fastest)
+grad_fastest = get_grad(R, Y_dists, Y, "fastest") 
+assert np.allclose(grad_none, grad_fast), "Gradient functions return different values"
+assert np.allclose(grad_none, grad_fastest), "Gradient functions return different values"
 
 print("Different levels of optimization are consistent.")
 
-###################################################################################
+###########################################################################################################################
 # Check that functions return correct output types
-################################################################################### 
-assert isinstance(D_none, np.ndarray) 
-assert isinstance(D_fastest, np.ndarray) 
-assert isinstance(P_none, np.ndarray) 
-assert isinstance(P_fastest, np.ndarray)
-assert isinstance(grad_none, np.ndarray)
-assert isinstance(grad_fast, np.ndarray)
-assert isinstance(grad_fastest, np.ndarray)
+###########################################################################################################################
+assert isinstance(D_none, np.ndarray), "Output of distance function with no optimization is not a numpy array"
+assert isinstance(D_fastest, np.ndarray), "Output of optimized distance function is not a numpy array" 
+assert isinstance(P_none, np.ndarray), "Output of p_ij function with no optimization is not a numpy array" 
+assert isinstance(P_fastest, np.ndarray), "Output of optimized p_ij function is not a numpy array"
+assert isinstance(grad_none, np.ndarray), "Output of gradient function with no optimization is not a numpy array"
+assert isinstance(grad_fast, np.ndarray), "Output of gradient function with some optimization is not a numpy array"
+assert isinstance(grad_fastest, np.ndarray), "Output of optimized gradient function is not a numpy array"
 
 tsne_d2 = tsne(X, niter = 3, verbose=False)
-assert isinstance(tsne_d2, np.ndarray) and tsne_d2.shape[-1] == 2
+assert isinstance(tsne_d2, np.ndarray), "Output of 2D t-SNE function is not a numpy array"
+assert tsne_d2.shape[-1] == 2, "Output of t-SNE function with these inputs should be two-dimensional"
 
 tsne_d3 = tsne(X, d=3,  niter = 3, verbose=False)
-assert isinstance(tsne_d3, np.ndarray) and tsne_d3.shape[-1] == 3
+assert isinstance(tsne_d3, np.ndarray), "Output of 3D t-SNE function is not a numpy array" 
+assert tsne_d3.shape[-1] == 3, "Output of t-SNE function with these inputs should be three-dimensional"
 
 print("All functions return the correct output")
