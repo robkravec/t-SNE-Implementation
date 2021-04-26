4e5e0fdb96bbeef15f5c03d6fe3b928e7d599911
Rob Kravec <rkravec17@gmail.com>
2021-04-26 21:40:57 +0000

    Rebuild

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tsne663',
-      version='0.9',
+      version='1.0',
       description='Implentation of the t-SNE algorithm for dimension reduction',
       long_description = 'Please see https://github.com/robkravec/STA663_Project for more information on this package',
       url='https://github.com/robkravec/STA663_Project',
@@ -9,5 +9,4 @@ setup(name='tsne663',
       author_email='marc.brooks@duke.edu, robert.kravec@duke.edu, steven.winter@duke.edu',
       license='MIT',
       packages=find_packages(exclude=['tests', 'test.*']),
-      install_requires=['matplotlib>=3.1.1', 'numpy>=1.18.1', 'numba>=0.48.0', 'tqdm>=4.42.0', 'scikit-learn>=0.22.1'],
-      py_modules=['tsne', 'sim'])
+      install_requires=['matplotlib>=3.1.1', 'numpy>=1.18.1', 'numba>=0.48.0', 'tqdm>=4.42.0', 'scikit-learn>=0.22.1'])
